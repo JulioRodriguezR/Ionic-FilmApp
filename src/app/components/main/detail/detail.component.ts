075fb0b41592d4641791a9c6d50be9fdfff46c51
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-06-08 11:30:46 +0200

    Show detail movie

--- src/app/components/main/detail/detail.component.ts
+++ src/app/components/main/detail/detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MoviesService } from 'src/app/services/movies.service';
-import { DetailMovie } from 'src/app/models/models';
+import { DetailMovie, Cast } from 'src/app/models/models';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detail',
@@ -10,21 +11,33 @@ import { DetailMovie } from 'src/app/models/models';
 export class DetailComponent implements OnInit {
   @Input() id;
   movie: DetailMovie;
+  actors: Cast[] = [];
   hidden = 100;
 
-  constructor(private moviesSrv: MoviesService) { }
+  slideOptActors = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spacebetween: -5
+  };
+
+  constructor(private moviesSrv: MoviesService, private modalCtr: ModalController) { }
 
   ngOnInit() {
     this.moviesSrv.getDetailMovie(this.id)
       .subscribe(resp => {
-        console.log({ resp });
         this.movie = resp;
       });
 
     this.moviesSrv.getActorsMovie(this.id)
       .subscribe(resp => {
-        console.log({ resp });
+        this.actors = resp.cast;
       });
   }
+
+  back() {
+    this.modalCtr.dismiss();
+  }
+
+  favorite() { }
 
 }
