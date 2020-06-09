2b6238d4812b9ecd0e89014f0f4958d4913ce54e
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-06-09 10:31:06 +0200

    Desing search page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  txtSearch = '';
+  points = ['CiberPunk', 'Spiderman', 'Tous le matins de monde'];
 
-  constructor() {}
+  constructor() { }
 
+  search(ev) {
+    const value = ev.detail.value;
+    console.log({ value });
+  }
 }
