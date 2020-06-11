8b27958da6df2e71ad730e5f05d09035e7b5bef4
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-06-11 17:45:26 +0200

    Desing search movie page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
+import { Film } from '../models/models';
+import { DetailComponent } from '../components/main/detail/detail.component';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -8,17 +11,39 @@ import { MoviesService } from '../services/movies.service';
 })
 export class Tab2Page {
   txtSearch = '';
-  points = ['CiberPunk', 'Spiderman', 'Tous le matins de monde'];
+  movies: Film[] = [];
+  points = ['Super Man', 'Spiderman', 'The Lord of the Ring'];
+  searching = false;
 
-  constructor(private moviesSrv: MoviesService) { }
+  constructor(private moviesSrv: MoviesService, private modalCtrl: ModalController) { }
 
   search(ev) {
     const value = ev.detail.value;
 
+    this.searching = true;
+
+    if (value.length === 0) {
+      this.searching = false;
+      this.movies = [];
+
+      return;
+    }
+
     this.moviesSrv.getSearchMovie(value)
       .subscribe(resp => {
-        console.log({ resp });
-
+        this.movies = resp['results'];
+        this.searching = false;
       });
+  }
+
+  async showDetail(id: string) {
+    const modal = await this.modalCtrl.create({
+      component: DetailComponent,
+      componentProps: {
+        id
+      }
+    });
+
+    modal.present();
   }
 }
