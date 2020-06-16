51c7704546f63abcae0cfa4a9114bf5343828d0e
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-06-16 17:39:56 +0200

    Favorite by genre

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -1,99 +1,104 @@
 export interface RootResponse {
-    page: number;
-    total_results: number;
-    total_pages: number;
-    results: Film[];
+  page: number;
+  total_results: number;
+  total_pages: number;
+  results: Film[];
 }
 
 export interface Film {
-    popularity: number;
-    vote_count: number;
-    video: boolean;
-    poster_path: string;
-    id: number;
-    adult: boolean;
-    backdrop_path?: string;
-    original_language: string;
-    original_title: string;
-    genre_ids: number[];
-    title: string;
-    vote_average: number;
-    overview: string;
-    release_date: string;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  poster_path: string;
+  id: number;
+  adult: boolean;
+  backdrop_path?: string;
+  original_language: string;
+  original_title: string;
+  genre_ids: number[];
+  title: string;
+  vote_average: number;
+  overview: string;
+  release_date: string;
 }
 export interface DetailFilm {
-    adult: boolean;
-    backdrop_path: string;
-    belongs_to_collection?: any;
-    budget: number;
-    genres: Genre[];
-    homepage: string;
-    id: number;
-    imdb_id: string;
-    original_language: string;
-    original_title: string;
-    overview: string;
-    popularity: number;
-    poster_path: string;
-    production_companies: Productioncompany[];
-    production_countries: Productioncountry[];
-    release_date: string;
-    revenue: number;
-    runtime: number;
-    spoken_languages: Spokenlanguage[];
-    status: string;
-    tagline: string;
-    title: string;
-    video: boolean;
-    vote_average: number;
-    vote_count: number;
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection?: any;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: Productioncompany[];
+  production_countries: Productioncountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: Spokenlanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
 
 export interface Spokenlanguage {
-    iso_639_1: string;
-    name: string;
+  iso_639_1: string;
+  name: string;
 }
 
 export interface Productioncountry {
-    iso_3166_1: string;
-    name: string;
+  iso_3166_1: string;
+  name: string;
 }
 
 export interface Productioncompany {
-    id: number;
-    logo_path?: string;
-    name: string;
-    origin_country: string;
+  id: number;
+  logo_path?: string;
+  name: string;
+  origin_country: string;
 }
 
 export interface Genre {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 export interface Credits {
-    id: number;
-    cast: Cast[];
-    crew: Crew[];
+  id: number;
+  cast: Cast[];
+  crew: Crew[];
 }
 
 export interface Crew {
-    credit_id: string;
-    department: string;
-    gender: number;
-    id: number;
-    job: string;
-    name: string;
-    profile_path?: string;
+  credit_id: string;
+  department: string;
+  gender: number;
+  id: number;
+  job: string;
+  name: string;
+  profile_path?: string;
 }
 
 export interface Cast {
-    cast_id: number;
-    character: string;
-    credit_id: string;
-    gender: number;
-    id: number;
-    name: string;
-    order: number;
-    profile_path?: string;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  gender: number;
+  id: number;
+  name: string;
+  order: number;
+  profile_path?: string;
+}
+
+export interface FavoriteGenre {
+  genre: string;
+  films: DetailFilm[];
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,25 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { DetailFilm, Genre } from '../models/models';
-import { DataLocalService } from '../services/data-local.service';
-import { MoviesService } from '../services/movies.service';
+import { Component } from "@angular/core";
+import { DetailFilm, FavoriteGenre, Genre } from "../models/models";
+import { DataLocalService } from "../services/data-local.service";
+import { MoviesService } from "../services/movies.service";
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  selector: "app-tab3",
+  templateUrl: "tab3.page.html",
+  styleUrls: ["tab3.page.scss"],
 })
-export class Tab3Page implements OnInit {
+export class Tab3Page {
   films: DetailFilm[] = [];
   genres: Genre[] = [];
+  favoriteGenre: FavoriteGenre[];
 
   constructor(
     private dataLocalSrv: DataLocalService,
     private movieSrv: MoviesService
-  ) { }
+  ) {}
 
-  async ngOnInit() {
+  async ionViewWillEnter() {
     this.films = await this.dataLocalSrv.loadFavorites();
     this.genres = await this.movieSrv.loadGenre();
+
+    this.filmsByGenre(this.genres, this.films);
   }
 
+  filmsByGenre(genres: Genre[], films: DetailFilm[]) {
+    this.favoriteGenre = [];
+
+    console.log({ genres });
+
+    genres.forEach((genre) => {
+      this.favoriteGenre.push({
+        genre: genre.name,
+        films: films.filter((film) => {
+          return film.genres.find((g) => g.id === genre.id);
+        }),
+      });
+    });
+  }
 }
